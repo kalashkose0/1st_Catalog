de569b9cc9ea26617662c308573600fa109869fe
kalashkose0 <kalashkose0@gmail.com>
2024-03-01 19:32:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         child: Text("Hello Gyes"),
+//       ),
+//     );
+//   }
+// }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Hello Gyes"),
+          ),
+        ),
+      ),
+    );
+  }
+}
